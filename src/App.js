dca1fc4349798f36be699b91cffa3934ebf41ef6
HimashaNethmini <himashanethmini05@gmail.com>
2023-04-05 13:21:55 +0530

    Updated App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import Hero from "./components/Hero/Hero";
+import Program from "./components/Program/Programs";
+import Reason from "./components/Reason/Reasons";
+import Plan from "./components/Plan/Plans";
+
 function App() {
   return (
     <div className="app">
-          <hero/>
+          <Hero/>
+          <Program/>
+          <Reason/>
+          <Plan/>
+
+
     </div>
   );
 }

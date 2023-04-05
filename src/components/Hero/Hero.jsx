22b9499ac355c024b3724e19a1583adc93b88937
HimashaNethmini <himashanethmini05@gmail.com>
2023-04-05 15:15:21 +0530

    Updated jsx files

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,23 +4,23 @@ import Header from '../Header/Header'
 import hero_image from "../../assets/hero_image.png";
 import hero_image_back from "../../assets/hero_image_back.png";
 import heart from "../../assets/heart.png";
-import Calories from "../../assets/Calories.png";
+import Calories from "../../assets/calories.png";
 
 
 const Hero = () => {
   return (
-    <div className='hero'>
-        <div className='left-h'>
+    <div className="hero">
+        <div className="left-h">
             <Header/>
   {/*the best ad*/}
-            <div className='the-best-ad'>
+            <div className="the-best-ad">
               <div></div>
               <span>the best fitness club in the town</span>
             </div>
   {/*Hero heading*/}
-            <div className='hero-text'>
+            <div className="hero-text">
               <div>
-                <span className='stroke-text'>Shape </span>
+                <span className="stroke-text">Shape </span>
                 <span>You</span>
               </div>
               <div>
@@ -33,7 +33,7 @@ const Hero = () => {
               </div>
             </div>
             {/*figures*/}
-            <div className='figures'>
+            <div className="figures">
               <div>
                 <span>+140</span>
                 <span>Expert Coachs</span>
@@ -48,20 +48,30 @@ const Hero = () => {
               </div>
             </div>
             {/* hero button*/}
-            <div className='hero-button'>
+            <div className="hero-button">
               <button className='btn'>Get Started</button>
               <button className='btn'>Learn More</button>
             </div>
         </div>
-        <div className='right-h'> 
+        <div className="right-h"> 
           <button className='btn'>Join Now</button>
 
-          <div className='heart-rate'>
+          <div className="heart-rate">
             <img src={heart} alt=''/>
             <span>Heart Rate </span>
             <span>116 bpm </span>
           </div>
-        
+          {/*hero images*/}
+          <img src={hero_image} alt='' className="hero-image"/>
+          <img src={hero_image_back} alt='' className="hero-image-back"/>
+          
+          {/*Calories*/}
+          <div className="Calories"><img src={Calories} alt=''/>
+            <div>
+              <span> Calories Burned </span>
+              <span>220 kcal</span>
+            </div>
+          </div>
 
         </div>
 
@@ -69,4 +79,4 @@ const Hero = () => {
   )
 }
 
-export default Hero
+export default Hero;
